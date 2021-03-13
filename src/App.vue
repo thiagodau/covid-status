c3d0121705f906293209e8aaa3f96d03cbbb4efc
= <=>
2021-03-13 19:55:53 -0400

    Consumindo api com sucesso

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from "../src/pages/Home";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+* h1 {
+  font-size: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #202124;
+  color: #e5e5e5;
 }
 </style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>COVID19 - Status by BrazilApi</h1>
+    </div>
+    <div class="dados">
+      <h1>Dados</h1>
+      <div class="dados-body">
+        <div class="lista">
+          <h1>Status Brasil por Estado (UF)</h1>
+          <table>
+            <tbody>
+              <tr>
+                <th>📍 Estado</th>
+                <th>🚨 Confirmados</th>
+                <th>💀 Mortes</th>
+              </tr>
+              <tr v-for="dado in dadosEstadosBrasil" :key="dado.id">
+                <td>{{ dado.uf }}</td>
+                <td>{{ dado.cases }}</td>
+                <td>{{ dado.deaths }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="card-brasil">
+          <h1>Status Geral Brasil</h1>
+          <ul>
+            <li>✅ {{ dadosBrasil.confirmed }} Confirmados</li>
+            <li>📖 {{ dadosBrasil.cases }} Casos</li>
+            <li>♻️ {{ dadosBrasil.recovered }} Recuperados</li>
+            <li>💀 {{ dadosBrasil.deaths }} Mortes</li>
+            <li>Atualizado em: {{ dadosBrasil.updated_at }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+let BASE_URL = "https://covid19-brazil-api.now.sh/api/report/";
+
+export default {
+  data() {
+    return {
+      dadosEstadosBrasil: null,
+      dadosBrasil: null,
+    };
+  },
+  mounted() {
+    this.getDadosEstadosBrasil();
+    this.getDadosBrasil();
+  },
+  methods: {
+    getDadosEstadosBrasil() {
+      axios
+        .get(BASE_URL + "v1")
+        .then((response) => {
+          this.dadosEstadosBrasil = response.data;
+          this.dadosEstadosBrasil = this.dadosEstadosBrasil.data;
+        })
+        .catch((e) => console.log(e));
+    },
+    getDadosBrasil() {
+      axios
+        .get(BASE_URL + "v1/brazil")
+        .then((response) => {
+          this.dadosBrasil = response.data;
+          this.dadosBrasil = this.dadosBrasil.data;
+          console.log(this.dadosBrasil);
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  padding: 30px;
+  display: block;
+}
+
+.dados {
+  display: block;
+  padding: 30px;
+}
+
+.dados-body {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 20px;
+}
+
+.lista {
+  background: #1a1a1d;
+  margin: 0 30px 0 30px;
+  padding: 30px;
+  border-radius: 30px;
+  border: 1px solid #1a1a1d;
+}
+
+.lista:hover {
+  border: 1px solid #4ffa7b;
+}
+
+.lista table {
+  width: 100%;
+  margin: 10px;
+  text-align: center;
+}
+
+.card-brasil {
+  background: #1a1a1d;
+  margin: 0 30px 0 30px;
+  padding: 30px;
+  border-radius: 30px;
+  line-height: 30px;
+  border: 1px solid #1a1a1d;
+}
+.card-brasil:hover {
+  border: 1px solid #4ffa7b;
+}
+
+.card-brasil ul {
+  list-style: none;
+  margin: 10px;
+}
+</style>
