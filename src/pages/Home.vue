a5cc0b5070bc4120d4f928d9f8f7ac7f9402870b
= <=>
2021-03-16 10:23:38 -0400

    Done v1

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,78 +2,25 @@
   <div>
     <div class="container">
       <h1>COVID19 - Status by BrazilApi</h1>
+      <img :src="imageCovid" />
     </div>
-    <div class="dados">
-      <h1>Dados</h1>
-      <div class="dados-body">
-        <div class="lista">
-          <h1>Status Brasil por Estado (UF)</h1>
-          <table>
-            <tbody>
-              <tr>
-                <th>📍 Estado</th>
-                <th>🚨 Confirmados</th>
-                <th>💀 Mortes</th>
-              </tr>
-              <tr v-for="dado in dadosEstadosBrasil" :key="dado.id">
-                <td>{{ dado.uf }}</td>
-                <td>{{ dado.cases }}</td>
-                <td>{{ dado.deaths }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="card-brasil">
-          <h1>Status Geral Brasil</h1>
-          <ul>
-            <li>✅ {{ dadosBrasil.confirmed }} Confirmados</li>
-            <li>📖 {{ dadosBrasil.cases }} Casos</li>
-            <li>♻️ {{ dadosBrasil.recovered }} Recuperados</li>
-            <li>💀 {{ dadosBrasil.deaths }} Mortes</li>
-            <li>Atualizado em: {{ dadosBrasil.updated_at }}</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <Dados />
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import imageCovid from "../assets/coronavirus.svg";
 
-let BASE_URL = "https://covid19-brazil-api.now.sh/api/report/";
+import Dados from "../components/Dados";
 
 export default {
-  data() {
+  data: function () {
     return {
-      dadosEstadosBrasil: null,
-      dadosBrasil: null,
+      imageCovid,
     };
   },
-  mounted() {
-    this.getDadosEstadosBrasil();
-    this.getDadosBrasil();
-  },
-  methods: {
-    getDadosEstadosBrasil() {
-      axios
-        .get(BASE_URL + "v1")
-        .then((response) => {
-          this.dadosEstadosBrasil = response.data;
-          this.dadosEstadosBrasil = this.dadosEstadosBrasil.data;
-        })
-        .catch((e) => console.log(e));
-    },
-    getDadosBrasil() {
-      axios
-        .get(BASE_URL + "v1/brazil")
-        .then((response) => {
-          this.dadosBrasil = response.data;
-          this.dadosBrasil = this.dadosBrasil.data;
-          console.log(this.dadosBrasil);
-        })
-        .catch((e) => console.log(e));
-    },
+  components: {
+    Dados,
   },
 };
 </script>
@@ -81,52 +28,12 @@ export default {
 <style>
 .container {
   padding: 30px;
-  display: block;
+  display: flex;
+  align-items: center;
 }
 
-.dados {
-  display: block;
-  padding: 30px;
-}
-
-.dados-body {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  padding: 20px;
-}
-
-.lista {
-  background: #1a1a1d;
-  margin: 0 30px 0 30px;
-  padding: 30px;
-  border-radius: 30px;
-  border: 1px solid #1a1a1d;
-}
-
-.lista:hover {
-  border: 1px solid #4ffa7b;
-}
-
-.lista table {
-  width: 100%;
-  margin: 10px;
-  text-align: center;
-}
-
-.card-brasil {
-  background: #1a1a1d;
-  margin: 0 30px 0 30px;
-  padding: 30px;
-  border-radius: 30px;
-  line-height: 30px;
-  border: 1px solid #1a1a1d;
-}
-.card-brasil:hover {
-  border: 1px solid #4ffa7b;
-}
-
-.card-brasil ul {
-  list-style: none;
-  margin: 10px;
+.container img {
+  width: 5%;
+  margin-left: 20px;
 }
 </style>
